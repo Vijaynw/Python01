15f97dc5c359e66ea438572079d524838c740e73
Vijaynw <38061521+Vijaynw@users.noreply.github.com>
2020-12-25 12:51:17 +0530

    Delete input.py

--- input.py
+++
@@ -1,9 +0,0 @@
-x=int(input("Enter the firdt number:"))
-y=int(input("Enter the second number:"))
-z=x+y
-print("The additio1n of two number is:",z)
-
-
-
-#for input one character    use indexing while printing    and while fetching the input after input use [0]
-# eval(input('Expersion')
